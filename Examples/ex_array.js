f96bb8ca48351fa11b1f3d2380308832c73f05b8
Antoine07 <antoine.lucsko@gmail.com>
2025-01-21 14:58:23 +0100

    ex array

---
+++ Examples/ex_array.js
@@ -0,0 +1,19 @@
+
+
+
+let numbers = [11, 16, 17, 18, 20]
+
+// numbers.map()
+
+// ces deux fonctions anomymes retournent des valeurs 1 et puissance d'un nombre
+let an = () => 1 ;
+let bn = (x) => x**2 ;
+
+console.log(an())
+
+console.log(bn(5))
+
+// Exercices 
+// Créez la fonction anomyme qui retourne une puissance de 3 d'un nombre quelconque
+// Essayez d'afficher les valeurs du tableau numbers avec un map 
+// Essayez de mettre à la puissance 3 chaque valeur du tableau numbers
