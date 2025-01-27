ee2e76a0f701ac7f6487c887f9441bebb22f1034
Antoine07 <antoine.lucsko@gmail.com>
2025-01-27 10:23:37 +0100

    asycn

--- Examples/example-async.js
+++ Examples/example-async.js
@@ -1,18 +1,26 @@
+
+// une fonction qui retourne une promesse 
 function simulateDelay(ms) {
+   // une promesse c'est un objet qui permet d'encapsuler une fonction asynchrone 
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   
   async function task() {
+    // synchrone dans une async / await 
     console.log('Start');
     
     // On attend 2 secondes avant de poursuivre
     await simulateDelay(2000);
     
+    // après on execute ce code
     console.log('End');
 
+    // ce que retourne la promesse
     return "Hello async"
   }
   
+  // then permet de récupérer le résulat de l'async/await qui est une promesse
   task().then(console.log)
 
+  // synchrone 
   console.log('Oups');
